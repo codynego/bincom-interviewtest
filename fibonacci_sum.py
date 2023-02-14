be5e614a2eae0b4f12fcd8264bf6b11a2bd5b2ad
Abednego Emonena <emonenaabednego@gmail.com>
2023-02-14 14:45:57 +0500

    added fibonaci sum file

--- fibonacci_sum.py
+++ fibonacci_sum.py
@@ -1,3 +1,11 @@
 # Find the sum of the first 50 fibonacci sequence numbers
+# 
+def fibonacci_sum(n=50):
+    # Initialize the fibonacci sequence
+    fibonacci_sequence = [0, 1]
+    # Find the sum of the first 50 fibonacci sequence numbers
+    for i in range(2, n):
+        fibonacci_sequence.append(fibonacci_sequence[i - 1] + fibonacci_sequence[i - 2])
+    return sum(fibonacci_sequence)
 
-
+print(fibonacci_sum())
